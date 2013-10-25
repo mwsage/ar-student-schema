c32ff1e653ff3f25915ac4d651a2d1fe9aa38b2f
Apprentice <apprentice@dbc01.local>
2013-10-24 23:12:59 -0700

    ar student schema finished all tests passing

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,28 @@ require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+
+    validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
+      #:message => "did not provide the proper email format" }
+    validates :email, :uniqueness => true
+    validates :age, :numericality => {:greater_than => 5}
+    validates :phone, :format => { :with => /.*\d{3}.*\d{3}.*\d{4}.*/}
+
+  def name
+    #Students.all.each do |student|
+      #student.name = [student.first_name, student.last_name].join('')
+      #student.save
+    #end
+    first_name + " " + last_name
+  end
+
+  def age
+   Time.now.year - birthday.year
+  end
+
+
 end
+
+
+
+
--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -5,5 +5,48 @@ require_relative '../config'
 class CreateStudents < ActiveRecord::Migration
   def change
     # HINT: checkout ActiveRecord::Migration.create_table
+    create_table :students do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string  :gender
+      t.date :birthday
+      t.string :email
+      t.string :phone
+
+
+      t.timestamps
+      # t.datetime :created_at
+      # t.datetime :updated_at
+    end
   end
 end
+    #   :integer => ["id"],
+    #   :string => ["first_name", "last_name", "gender", "email", "phone"],
+    #   :date => ["birthday"],
+    #   :datetime => ["created_at", "updated_at"]
+    # }
+
+# class AddNameColumn < ActiveRecord::migration
+#   def up
+#     add_column :student :name, :string, default: ''
+
+#     Students.all.each do |student|
+#       student.name = [student.first_name, student.last_name].join('')
+#       student.save
+#     end
+
+#     remove_column :first_name
+#     remove_column :last_name
+#   end
+
+#   def down
+#     add_column :student :first_name, :string, default: ''
+#     add_column :student :last_name, :string, default: ''
+  
+#     Students.all.each do |student|
+#       student.first_name, student.last_name = student.name.split('')
+#       student.save
+#     end
+#   remove_column :student :name 
+#   end
+# end
